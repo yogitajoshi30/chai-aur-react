2ff7da5cd72e47f66da62f0628e24649e1cdaf13
yogitajoshi30 <joshiyogita67@gmail.com>
2024-04-03 21:04:54 +0530

    interview question added

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,6 +7,21 @@ function App() {
   const addValue = () => {
     if (count < 20) setCount(count + 1);
     else setCount(count);
+    //INTERVIEW QUESTION: WHAT WILL HAPPEN WHEN THE SAME OPERATION IS PERFORMED 3-4 TIMES CONTINUOUSLY ?
+    /*
+    setCount(count + 1)
+    setCount(count + 1)
+    setCount(count + 1)
+    setCount(count + 1)
+    */
+    //useState updates are asynchronous, calling setCount multiple times in succession with the same state value (count) may not behave as expected. 
+    //Instead, you should use the functional update form of setCount when the new state depends on the previous state.
+    /*
+     setCount(count=> count+1)
+     setCount(count=> count+1)
+     setCount(count=> count+1)
+     setCount(count=> count+1)
+ */
   }
   const removeValue = () => {
     if (count > 0) setCount(count - 1);

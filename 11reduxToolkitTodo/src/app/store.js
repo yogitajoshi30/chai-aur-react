051eb352ee5c4c6d25c2de508deb4319153cdd35
yogitajoshi30 <joshiyogita67@gmail.com>
2024-04-19 22:17:27 +0530

    Redux toolkit

---
+++ 11reduxToolkitTodo/src/app/store.js
@@ -0,0 +1,7 @@
+//Step 1- Configure Store
+import { configureStore } from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice'
+
+export const store = configureStore({
+    reducer: todoReducer 
+})
